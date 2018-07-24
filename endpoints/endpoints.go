108a6a78ea436d9991051b9f3ebd683feded77f5
MacPro2011 <mbp2011@MacPro2011s-MacBook-Pro.local>
2018-07-25 01:34:49 +0700

    Modify name hieunmce by hiepdangnguyendai

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -2,9 +2,9 @@ package endpoints
 
 import (
 	"github.com/go-kit/kit/endpoint"
-	"github.com/hieunmce/example-go/service"
+	"github.com/hiepdangnguyendai/example-go/service"
 
-	"github.com/hieunmce/example-go/endpoints/user"
+	"github.com/hiepdangnguyendai/example-go/endpoints/user"
 )
 
 // Endpoints .
--- http/http.go
+++ http/http.go
@@ -9,8 +9,8 @@ import (
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 
-	"github.com/hieunmce/example-go/endpoints"
-	userDecode "github.com/hieunmce/example-go/http/decode/json/user"
+	"github.com/hiepdangnguyendai/example-go/endpoints"
+	userDecode "github.com/hiepdangnguyendai/example-go/http/decode/json/user"
 )
 
 // NewHTTPHandler ...
