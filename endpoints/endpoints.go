28c5bc95e05a81a8dd3f7c7c3c4f30c4dee07761
MacPro2011 <mbp2011@MacPro2011s-MacBook-Pro.local>
2018-07-25 15:30:50 +0700

    modify endpoints for book

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-kit/kit/endpoint"
 	"github.com/hiepdangnguyendai/example-go/service"
 
+	"github.com/hiepdangnguyendai/example-go/endpoints/book"
 	"github.com/hiepdangnguyendai/example-go/endpoints/category"
 	"github.com/hiepdangnguyendai/example-go/endpoints/user"
 )
@@ -22,6 +23,12 @@ type Endpoints struct {
 	CreateCategory    endpoint.Endpoint
 	UpdateCategory    endpoint.Endpoint
 	DeleteCategory    endpoint.Endpoint
+	// Book Endpoint
+	FindBook     endpoint.Endpoint
+	FindAllBooks endpoint.Endpoint
+	CreateBook   endpoint.Endpoint
+	UpdateBook   endpoint.Endpoint
+	DeleteBook   endpoint.Endpoint
 }
 
 // MakeServerEndpoints returns an Endpoints struct
@@ -39,5 +46,11 @@ func MakeServerEndpoints(s service.Service) Endpoints {
 		CreateCategory:    category.MakeCreateEndpoint(s),
 		UpdateCategory:    category.MakeUpdateEndpoint(s),
 		DeleteCategory:    category.MakeDeleteEnpoint(s),
+		// Book Enpoint
+		FindBook:     book.MakeFindEndPoint(s),
+		FindAllBooks: book.MakeFindAllEndpoint(s),
+		CreateBook:   book.MakeCreateEndpoint(s),
+		UpdateBook:   book.MakeUpdateEndpoint(s),
+		DeleteBook:   book.MakeDeleteEnpoint(s),
 	}
 }
